54f70d990aa53bed48f2fbde0254bcc80622b913
bakryabdalsalam <aymanbeko66@mail.com>
2023-09-04 14:30:30 +0300

    add dynamic date and api route

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 
 JOB = [
     {
-        id: 1,
+        'id': 1,
         'title': 'Software Engineer',
         'location': 'San Francisco, CA',
         'salary': '$100,000',
     },
     {
-        id: 2,
+        'id': 2,
         'title': 'data scientist',
         'location': 'cairo, egypt',
         'salary': '$50,000',
     },
     {
-        id: 3,
+        'id': 3,
         'title': 'data engineer',
         'location': 'USA, CA',
         'salary': '$150,000',
     },
     {
-        id: 4,
+        'id': 4,
         'title': 'front end developer',
         'location': 'UAE, Dubai',
         'salary': '$200,000',
-    },
+    }
         
 ]
 
 @app.route('/')
 def hello_world():
     return render_template('home.html', jobs=JOB, campany_name="Bakry")
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOB)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
