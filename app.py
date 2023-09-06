b0bf22e1413d60fe956ebd42df8b91b50e9c99a9
bakryabdalsalam <aymanbeko66@mail.com>
2023-09-06 23:57:22 +0300

    update sql

--- app.py
+++ app.py
@@ -1,42 +1,17 @@
 from flask import Flask, render_template, jsonify
-
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOB = [
-    {
-        'id': 1,
-        'title': 'Software Engineer',
-        'location': 'San Francisco, CA',
-        'salary': '$100,000',
-    },
-    {
-        'id': 2,
-        'title': 'data scientist',
-        'location': 'cairo, egypt',
-        'salary': '$50,000',
-    },
-    {
-        'id': 3,
-        'title': 'data engineer',
-        'location': 'USA, CA',
-        'salary': '$150,000',
-    },
-    {
-        'id': 4,
-        'title': 'front end developer',
-        'location': 'UAE, Dubai',
-        'salary': '$200,000',
-    }
-        
-]
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', jobs=JOB, campany_name="Bakry")
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, campany_name="Bakry")
 
 @app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOB)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
